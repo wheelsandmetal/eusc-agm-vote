668c8ceebb2ea94460f6c570676862050051d4c0
wheelsandmetal <jakob@schmutz.co.uk>
2019-02-04 22:26:54 +0000

    css style for frontend
    Co-authored-by: Tom Martin <tom@tommart.in>

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/election/", electionHandler)
 	http.HandleFunc("/vote/", voteHandler)
