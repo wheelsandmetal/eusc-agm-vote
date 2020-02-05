3abaee68ccd035b057c9316f8a504ca9628a09cc
wheelsandmetal <jakob@schmutz.co.uk>
2020-02-05 00:53:29 +0000

    update to go111 and use cloud.google.com/go/datastore

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"cloud.google.com/go/datastore"
 	"fmt"
+	"google.golang.org/api/iterator"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 	"sort"
 	"strings"
-	"html/template"
-	"net/http"
-	"google.golang.org/appengine" // Required external App Engine library
-	"google.golang.org/appengine/log"
-	"google.golang.org/appengine/datastore"
 )
 
+const PROJECT_ID string = "eusc-agm-vote"
+const AUTH_FILE string = "auth-file.json"
+
 func main() {
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", AUTH_FILE)
+
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/election/", electionHandler)
 	http.HandleFunc("/vote/", voteHandler)
-	appengine.Main() // Starts the server to receive requests
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,19 +42,24 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ctx := appengine.NewContext(r)
+	ctx := r.Context()
+	client, err := datastore.NewClient(ctx, PROJECT_ID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
 	q := datastore.NewQuery("Election").Order("Order")
 
 	var elections []Election
 	var election Election
-	it := q.Run(ctx)
-	_, err := it.Next(&election)
+	it := client.Run(ctx, q)
+	_, err = it.Next(&election)
 	for err == nil {
 		elections = append(elections, election)
 		_, err = it.Next(&election)
 	}
-	if err != datastore.Done {
-		message := fmt.Sprintf("Failed fetching results", err)
+	if err != iterator.Done {
+		message := fmt.Sprintf("Failed fetching results ", err)
 		errorHandler(w, r, http.StatusNotFound, message, err)
 	}
 
@@ -58,16 +79,21 @@ func electionHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ctx := appengine.NewContext(r)
+	ctx := r.Context()
+	client, err := datastore.NewClient(ctx, PROJECT_ID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
 	q := datastore.NewQuery("Election").
 		Filter("Key =", pos_key)
 
 	var election Election
-	it := q.Run(ctx)
-	_, err := it.Next(&election)
+	it := client.Run(ctx, q)
+	_, err = it.Next(&election)
 	if err != nil || len(election.Candidates_Keys) < 1 {
 		message := fmt.Sprintf("Failed to get any candidates")
-		log.Errorf(ctx, "Failed fetching results: %v", err)
+		log.Fatalf("Failed fetching results: %v", err)
 		errorHandler(w, r, http.StatusNotFound, message, err)
 		return
 	}
@@ -75,14 +101,14 @@ func electionHandler(w http.ResponseWriter, r *http.Request) {
 	if len(election.Candidates_Keys) >= 1 {
 		var candidates = make([]Candidate, len(election.Candidates_Keys))
 
-		err = datastore.GetMulti(ctx, election.Candidates_Keys, candidates)
+		err = client.GetMulti(ctx, election.Candidates_Keys, candidates)
 
 		if err != nil {
 			message := "No Candidates Found."
 			errorHandler(w, r, http.StatusNotFound, message, err)
 			return
 		}
-		sort.Slice(candidates, func(i, j int) bool { return candidates[i].Key < candidates[j].Key } )
+		sort.Slice(candidates, func(i, j int) bool { return candidates[i].Key < candidates[j].Key })
 
 		election.Candidates = candidates
 		t, _ := template.ParseFiles("tmpl/vote.html")
@@ -97,20 +123,24 @@ func voteHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
+	ctx := r.Context()
+	client, err := datastore.NewClient(ctx, PROJECT_ID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
 
-	err := r.ParseForm()
+	err = r.ParseForm()
 	if err != nil {
 		panic(err)
 	}
 
 	voter_id := r.Form.Get("voter_id")
-	ctx := appengine.NewContext(r)
 
 	// Check voter_id exist in datastore
 	q := datastore.NewQuery("Voter").
 		Filter("Voter_ID =", voter_id).Limit(1)
 	var voter Voter
-	it := q.Run(ctx)
+	it := client.Run(ctx, q)
 	_, err = it.Next(&voter)
 	if err != nil {
 		message := fmt.Sprintf("No voter found with this ID. Are you sure you entered your identity correctly?")
@@ -124,7 +154,7 @@ func voteHandler(w http.ResponseWriter, r *http.Request) {
 		Filter("Key =", election_key).
 		Limit(1)
 	var election Election
-	it = q.Run(ctx)
+	it = client.Run(ctx, q)
 	_, err = it.Next(&election)
 	if err != nil || election.Active == false {
 		message := fmt.Sprintf("This current Election is NOT open right now.")
@@ -146,28 +176,28 @@ func voteHandler(w http.ResponseWriter, r *http.Request) {
 		Filter("Election_Key =", election_key).
 		Limit(1)
 	var vote Vote
-	it = q.Run(ctx)
+	it = client.Run(ctx, q)
 	key, err := it.Next(&vote)
 	if err == nil {
 		// Overwrite vote
 		vote.Candidate_Key = candidate_key
-		if _, err := datastore.Put(ctx, key, &vote); err != nil {
+		if _, err := client.Put(ctx, key, &vote); err != nil {
 			message := fmt.Sprintf("Unable to put vote in DataStore")
-			log.Errorf(ctx, "datastore.Put: %v", err)
+			log.Fatalf("datastore.Put: %v", err)
 			errorHandler(w, r, http.StatusInternalServerError, message, err)
 			return
 		}
 	} else {
 		// Create new vote
 		vote = Vote{
-			Voter_ID: voter.Voter_ID,
-			Election_Key: election_key,
+			Voter_ID:      voter.Voter_ID,
+			Election_Key:  election_key,
 			Candidate_Key: candidate_key,
 		}
-		key = datastore.NewIncompleteKey(ctx, "Vote", nil)
-		if _, err := datastore.Put(ctx, key, &vote); err != nil {
+		key = datastore.IncompleteKey("Vote", nil)
+		if _, err := client.Put(ctx, key, &vote); err != nil {
 			message := fmt.Sprintf("Unable to put vote in DataStore")
-			log.Errorf(ctx, "datastore.Put: %v", err)
+			log.Fatalf("datastore.Put: %v", err)
 			errorHandler(w, r, http.StatusInternalServerError, message, err)
 			return
 		}
@@ -177,29 +207,29 @@ func voteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 type Vote struct {
-	Voter_ID string
-	Election_Key string
+	Voter_ID      string
+	Election_Key  string
 	Candidate_Key string
 }
 
 type Voter struct {
-	Voter_ID string
+	Voter_ID  string
 	Vote_Keys []*datastore.Key
 }
 
 type Candidate struct {
-	Key string
-	Name string
+	Key     string
+	Name    string
 	Message string
 }
 
 type Election struct {
-	Key string
-	Position string
+	Key             string
+	Position        string
 	Candidates_Keys []*datastore.Key
-	Candidates []Candidate
-	Active bool
-	Order int
+	Candidates      []Candidate
+	Active          bool
+	Order           int
 }
 
 type templateParams struct {
@@ -207,29 +237,29 @@ type templateParams struct {
 }
 
 type ErrorParams struct {
-	Title string
+	Title       string
 	TypeMessage string
-	Message string
-	Error error
+	Message     string
+	Error       error
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request, status int, message string, err error) {
-    w.WriteHeader(status)
+	w.WriteHeader(status)
 	var errorParams ErrorParams
 
 	switch status {
-		case http.StatusBadRequest:
-			errorParams.Title = "400 BAD REQUEST"
-			errorParams.TypeMessage = "The server returned a 400 code"
-		case http.StatusNotFound:
-			errorParams.Title = "404 NOT FOUND"
-			errorParams.TypeMessage = "The server returned a 404 code"
-		case http.StatusInternalServerError:
-			errorParams.Title = "500 INTERNAL SERVER ERROR"
-			errorParams.TypeMessage = "The server returned a 500 code"
-		case http.StatusForbidden:
-			errorParams.Title = "403 FORBIDDEN"
-			errorParams.TypeMessage = "The server returned a 403 code"
+	case http.StatusBadRequest:
+		errorParams.Title = "400 BAD REQUEST"
+		errorParams.TypeMessage = "The server returned a 400 code"
+	case http.StatusNotFound:
+		errorParams.Title = "404 NOT FOUND"
+		errorParams.TypeMessage = "The server returned a 404 code"
+	case http.StatusInternalServerError:
+		errorParams.Title = "500 INTERNAL SERVER ERROR"
+		errorParams.TypeMessage = "The server returned a 500 code"
+	case http.StatusForbidden:
+		errorParams.Title = "403 FORBIDDEN"
+		errorParams.TypeMessage = "The server returned a 403 code"
 	}
 
 	errorParams.Message = message
